a41a77390b513ecdbd7918a3862f461669c2006e
alireza10hp <45386926+alireza10hp@users.noreply.github.com>
2022-12-19 08:54:20 +0330

    download logic

---
+++ app/src/main/java/com/example/myapplication/adapters/CategoryAdapter.java
@@ -0,0 +1,80 @@
+package com.example.myapplication.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.R;
+import com.example.myapplication.StartQuizActivity;
+import com.example.myapplication.models.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+
+    private List<Category> categoryList;
+    private Context context;
+    private Activity activity;
+
+    public CategoryAdapter(List<Category> categoryList, Activity activity) {
+        this.categoryList = categoryList;
+        this.activity=activity;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView text;
+        public LinearLayout layout;
+        public ImageView image;
+        public CardView cardView;
+
+        MyViewHolder(View view) {
+            super(view);
+            text=view.findViewById(R.id.text);
+            layout=view.findViewById(R.id.layout);
+            image=view.findViewById(R.id.image);
+            cardView=view.findViewById(R.id.card);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_category, parent, false);
+        context=parent.getContext();
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
+        final Category category = categoryList.get(position);
+        holder.layout.setBackgroundColor(Color.parseColor(category.getColor()));
+
+        Glide.with(context)
+                .load(category.getImage())
+                .into(holder.image);
+
+        holder.text.setText(category.getText());
+        holder.cardView.setOnClickListener(v -> {
+            StartQuizActivity.startActivity(context,String.valueOf(category.getId()),category.getText(),category.getColor(),category.getImage());
+            activity.overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+}
